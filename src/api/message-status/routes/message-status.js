718cff65ef945e0004eb520f2407621ed264dfcf
Jose Masri <ae_jmsalame@contractor.indeed.com>
2025-06-28 18:53:50 -0600

    refactor: update custom routes definition for message-status API

--- src/api/message-status/routes/message-status.js
+++ src/api/message-status/routes/message-status.js
@@ -6,42 +6,44 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
+// Create default router for CRUD operations
 const defaultRouter = createCoreRouter('api::message-status.message-status');
 
-const customRoutes = {
-  routes: [
-    {
-      method: 'GET',
-      path: '/message-statuses/message/:messageId',
-      handler: 'message-status.findByMessageId'
-    },
-    {
-      method: 'GET',
-      path: '/message-statuses/phone/:phoneNumber',
-      handler: 'message-status.findByPhoneNumber'
-    },
-    {
-      method: 'GET',
-      path: '/message-statuses/guest/:guestId',
-      handler: 'message-status.findByGuest'
-    },
-    {
-      method: 'PUT',
-      path: '/message-statuses/update/:messageId',
-      handler: 'message-status.updateStatus'
-    },
-    {
-      method: 'GET',
-      path: '/message-statuses/stats/event/:eventId',
-      handler: 'message-status.getEventStats'
-    }
-  ]
-};
+// Define custom routes
+const customRoutes = [
+  {
+    method: 'GET',
+    path: '/message-statuses/message/:messageId',
+    handler: 'message-status.findByMessageId'
+  },
+  {
+    method: 'GET',
+    path: '/message-statuses/phone/:phoneNumber',
+    handler: 'message-status.findByPhoneNumber'
+  },
+  {
+    method: 'GET',
+    path: '/message-statuses/guest/:guestId',
+    handler: 'message-status.findByGuest'
+  },
+  {
+    method: 'PUT',
+    path: '/message-statuses/update/:messageId',
+    handler: 'message-status.updateStatus'
+  },
+  {
+    method: 'GET',
+    path: '/message-statuses/stats/event/:eventId',
+    handler: 'message-status.getEventStats'
+  }
+];
 
-// Merge default routes with custom routes
+// Export combined routes
 module.exports = {
   routes: [
+    // Default CRUD routes
     ...defaultRouter.routes,
-    ...customRoutes.routes
+    // Custom routes
+    ...customRoutes
   ]
 };

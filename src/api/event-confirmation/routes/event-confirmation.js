37784db9dc80805d5c18e8b176196ac3f472e772
Jose Masri <ae_jmsalame@contractor.indeed.com>
2025-06-28 19:27:26 -0600

    refactor: reorganize event-confirmation routes for improved clarity and structure

--- src/api/event-confirmation/routes/event-confirmation.js
+++ src/api/event-confirmation/routes/event-confirmation.js
@@ -4,10 +4,60 @@
  * event-confirmation router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
 module.exports = {
   routes: [
+    // Core CRUD routes
+    {
+      method: 'GET',
+      path: '/event-confirmations',
+      handler: 'event-confirmation.find',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'GET',
+      path: '/event-confirmations/:id',
+      handler: 'event-confirmation.findOne',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'POST',
+      path: '/event-confirmations',
+      handler: 'event-confirmation.create',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'PUT',
+      path: '/event-confirmations/:id',
+      handler: 'event-confirmation.update',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'DELETE',
+      path: '/event-confirmations/:id',
+      handler: 'event-confirmation.delete',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+    // Custom routes
     {
       method: 'GET',
       path: '/event-confirmations/find/:guestId/:eventId',

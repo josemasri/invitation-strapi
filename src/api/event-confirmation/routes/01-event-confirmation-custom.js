f620724f15ba068805d31fc742bcce110690819c
Jose Masri <ae_jmsalame@contractor.indeed.com>
2025-06-28 19:41:25 -0600

    refactor: separate custom event-confirmation routes into a new file for better organization

---
+++ src/api/event-confirmation/routes/01-event-confirmation-custom.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * Custom event-confirmation routes
+ */
+
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/event-confirmations/find/:guestId/:eventId',
+      handler: 'event-confirmation.findByGuestAndEvent',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'GET',
+      path: '/event-confirmations/guest/:guestId',
+      handler: 'event-confirmation.findByGuest',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'GET',
+      path: '/event-confirmations/event/:eventId',
+      handler: 'event-confirmation.findByEvent',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'POST',
+      path: '/event-confirmations/confirm/:guestId/:eventId',
+      handler: 'event-confirmation.confirmForEvent',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+};
